983b1543887a52f16a15f127498178d304ed2617
naufalHaidar12342 <111201912342@mhs.dinus.ac.id>
2021-10-28 11:11:48 +0700

    memisahkan tombol dan mengubah button jadi button yang hanya berisi icon

--- app/src/main/java/com/example/biodatasederhana/MainActivity.java
+++ app/src/main/java/com/example/biodatasederhana/MainActivity.java
@@ -6,21 +6,47 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends AppCompatActivity {
-
+    Button setMaps, setPhone, setEmail;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //menyambungkan button di xml dengan logic di java
+        setMaps=findViewById(R.id.mapsBtn);
+        setEmail=findViewById(R.id.emailBtn);
+        setPhone=findViewById(R.id.kontakButton);
 
+        //set onclicklistener untuk tiap button
+        setMaps.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openGmaps();
+            }
+        });
+
+        setPhone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openPhone();
+            }
+        });
+
+        setEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openGmail();
+            }
+        });
     }
 
     //fungsi untuk membuka phone app
     //menggunakan Implicit Intent
-    public void openPhone(View passedView){
+    public void openPhone(){
         Uri callNumber=Uri.parse("tel:0811222333444");
         Intent callIntent=new Intent(Intent.ACTION_DIAL,callNumber);
         startActivity(callIntent);
@@ -28,7 +54,7 @@ public class MainActivity extends AppCompatActivity {
 
     //fungsi untuk membuka email app
     //menggunakan Implicit Intent
-    public void openGmail(View passedView){
+    public void openGmail(){
         Uri tujuanEmail=Uri.parse("mailto:111201912342@mhs.dinus.ac.id");
         String subjek="coba email";
         Intent callIntent=new Intent(Intent.ACTION_SENDTO,tujuanEmail);
@@ -38,7 +64,7 @@ public class MainActivity extends AppCompatActivity {
 
     //fungsi untuk membuka mail apps
     //menggunakan Implicit Intent
-    public void openGmaps(View view){
+    public void openGmaps(){
         //z adalah level zoom. paling tinggi nilainya=23
         String latnLong="@-7.0258396,110.4697273,15z";
         String location="Pedurungan+Kidul,+Kec.+Pedurungan,+Kota+Semarang,+Jawa+Tengah";
